844761a2bef9ad629204570d810416196e15f8ca
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-15 00:51:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,23 +11,24 @@ def nyc_pigeon_organizer(data)
   array_of_pigeons.flatten.uniq.each do |array|
     new_hash[array] = {}
     data.each do |keys, values|
-      values.each do |values, info|
-        new_hash[array][values] = []
-      end
+        new_hash[array][keys] = []
     end
   end
 
-
-new_hash
-end
-
-new_hash.each do |name, pigeon_atts|
-    data.each do |attribute, info|
-      info.each do |info, pigeon|
-        if pigeon.include?(name)
-           new_hash[name][pigeon_atts] = attribute
-
+  new_hash.each do |name, pigeon_atts|
+    pigeon_atts.each do |pigeon_atts, stats|
+      attribute1 = pigeon_atts
+      data.each do |attribute, info|
+        if attribute1 == attribute
+        info.each do |info, pigeon|
+          if pigeon.include?(name)
+             new_hash[name][pigeon_atts] << info
+          end
+        end
+        end
         end
       end
     end
-  end
+
+new_hash
+end
