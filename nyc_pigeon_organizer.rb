9fcf912e7b755c2d4c8cf294d21b281c62d5efe1
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-15 00:05:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new = {}
+  new2 = []
+  data.each do |keys, values|
+    values.each do |values, info|
+      new2.push(info)
+    end
+  end
+
+  new2.flatten.uniq.each do |array|
+    new[array] = {}
+  end
+
+  new2.each do |name|
+    name.each do |pigeon_att, pigeon_dat|
+      data.each do |attribute, info|
+        att = attribute
+          info.each do |data, pigeon|
+            if pigeon.include?(name)
+              new2
+            end
+            end
+          end
+        end
+      end
+
 end
